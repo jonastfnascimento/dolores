fb400bc67f86903c5c4880d4a1563f6935c35f90
gustavodot <gustavo.borges@dotgroup.com.br>
2025-01-14 21:08:27 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Dolores</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 4rem;
+  font-style: italic;
+  position: relative;
+  top: -10px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 100vh;
+}
+</style>
