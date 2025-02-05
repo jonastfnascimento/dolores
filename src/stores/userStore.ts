435f181396d58522edd11fe69024c153ad082f5a
Leonardo Freedy <leonardo.freedy@uappi.com.br>
2025-02-04 23:18:32 -0300

    fix: dados do usuario beforeEach do router

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -2,29 +2,34 @@ import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 import type { User } from '@/types/userTypes';
 
-export const useUserStore = defineStore(
-  'user',
-  () => {
-    const user = ref<User | null>(null);
-    const isAuthenticated = ref(false);
+export const useUserStore = defineStore('user', () => {
+  const user = ref<User | null>(null);
+  const isAuthenticated = ref(false);
 
-    const getUser = computed<User | null>(() => {
-      return user.value;
-    });
+  const getUser = computed<User | null>(() => {
+    return user.value;
+  });
 
-    const setUser = (userData: User) => {
-      user.value = userData;
-      isAuthenticated.value = true;
-    };
+  const setUser = (userData: User) => {
+    user.value = userData;
+    isAuthenticated.value = true;
 
-    const logout = () => {
-      user.value = null;
-      isAuthenticated.value = false;
-    };
+    localStorage.setItem('current_user', JSON.stringify(userData));
+  };
 
-    return { getUser, isAuthenticated, setUser, logout };
-  },
-  {
-    persist: true,
+  const logout = () => {
+    user.value = null;
+    isAuthenticated.value = false;
+  };
+
+  function authorization() {
+    const currentUser = localStorage.getItem('current_user');
+
+    if (!currentUser) return;
+
+    setUser(JSON.parse(currentUser));
+    isAuthenticated.value = true;
   }
-);
+
+  return { getUser, isAuthenticated, setUser, logout, authorization };
+});
